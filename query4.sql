b289170a9a7913555267bf663e6a0b195f9ecc19
gitbeatriz123 <velayosbea@gmail.com>
2025-02-16 16:38:31 +0100

    Add files via upload

---
+++ query4.sql
@@ -0,0 +1,11 @@
+select
+    calls_ivr_id,
+    vdn_label,
+    case
+        when vdn_label like 'ATC%' then 'FRONT'
+        when vdn_label like 'TECH%' then 'TECH'
+        when vdn_label = 'ABSORPTION' then 'ABSORPTION'
+        else 'RESTO'
+    end as vdn_aggregation
+from 
+    your_table_name;
