1897b13e322bd4315359b57f8add0eddec00737d
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 20:16:44 +0100

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -4,38 +4,38 @@ DROP TABLE IF EXISTS Bootcamp;
 DROP TABLE IF EXISTS Matricula;
 
 CREATE TABLE Profesor (
-    id_profesor serial primary key,
-    nombre varchar(50) not null,
-    apellidos varchar(50) not null,
-    telefono varchar(15) not null,
-    email varchar(100) unique not null
+    id_profesor serial PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    apellidos varchar(50) NOT NULL,
+    telefono varchar(15) NOT NULL,
+    email varchar(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE  Alumno (
-    id_alumno serial primary key,
-    nombre varchar(50) not null,
-    apellidos varchar(50) not null,
-    telefono varchar(15) not null,
-    email varchar(100) unique not null
+    id_alumno serial PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    apellidos varchar(50) NOT NULL,
+    telefono varchar(15) NOT NULL,
+    email varchar(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE Bootcamp (
-    id_bootcamp serial primary key,
-    nombre varchar(50) not null,
-    id_profesor int not null,
-    duracion int not null,
-    numero_alumnos int not null,
-    foreign key (id_profesor) references Profesor(id_profesor)
-    foreign key (id_alumno) references Alumno(id_alumno)
-    foreign key (id_bootcamp) references Bootcamp(id_bootcamp)
+    id_bootcamp serial PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    id_profesor int NOT NULL,
+    duracion int NOT NULL,
+    numero_alumnos int NOT NULL,
+    FOREIGN KEY (id_profesor) REFERENCES Profesor(id_profesor)
+    FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
+    FOREIGN KEY (id_bootcamp) REFERENCES Bootcamp(id_bootcamp)
     
     
 );
 CREATE TABLE Matricula (
-    id_matricula serial primary key,
-    id_alumno int not null,
-    pago_al_contado boolean not null,
-    pago_fraccionado boolean not null,
-    estado_de_la_matricula varchar(50) not null,
-    foreign key (id_alumno) references Alumno(id_alumno)
+    id_matricula serial PRIMARY KEY,
+    id_alumno int NOT NULL,
+    pago_al_contado boolean NOT NULL,
+    pago_fraccionado boolean NOT NULL,
+    estado_de_la_matricula varchar(50) NOT NULL,
+    FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
 );
