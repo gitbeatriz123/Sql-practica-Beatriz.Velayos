2e190162207a9e74273a5d9e32d8cde0937e181f
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 19:30:33 +0100

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -1,20 +1,4 @@
-select 
-    ivr_calls.ivr_id,
-    ivr_calls.phone_number,
-    ivr_calls.ivr_result,
-    ivr_calls.vdn_label,
-    ivr_calls.start_date,
-    ivr_calls.end_date,
-    ivr_calls.total_duration,
-    ivr_calls.customer_segment,
-    ivr_calls.ivr_language,
-    ivr_calls.steps_module,
-    ivr_calls.module_aggregation,
-    case 
-        when ivr_steps.step_name = 'CUSTOMERINFOBYDNI.TX' and ivr_steps.step_result = 'OK' then 1
-        else 0
-    end as info_by_dni_lg
-from 
-    ivr_calls
-left join 
-    ivr_steps on ivr_calls.ivr_id = ivr_steps.ivr_id;
+SELECT ivr_id
+     , MAX(IF(step_name = 'CUSTOMERINFOBYDNI.TX' AND step_result = 'OK', 1, 0)) AS info_by_dni_lg
+  FROM keepcoding.ivr_detail
+ GROUP BY ivr_id
