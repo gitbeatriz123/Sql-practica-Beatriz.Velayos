824e35ec8dfa25076ad12d24d11a3ba1f602a945
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 19:37:20 +0100

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,11 +1,7 @@
-select
-    calls_ivr_id,
-    vdn_label,
-    case
-        when vdn_label like 'ATC%' then 'FRONT'
-        when vdn_label like 'TECH%' then 'TECH'
-        when vdn_label = 'ABSORPTION' then 'ABSORPTION'
-        else 'RESTO'
-    end as vdn_aggregation
-from
-    keepcoding.ivr_calls calls
+SELECT calls.ivr_id
+     , CASE WHEN LEFT(calls.vdn_label, 3) = 'ATC' THEN 'FRONT'
+            WHEN LEFT(calls.vdn_label, 4) = 'TECH' THEN 'TECH'
+            WHEN calls.vdn_label = 'ABSORPTION' THEN 'ABSORPTION'
+            ELSE 'RESTO'
+       END AS vdn_aggregation
+  FROM `keepcoding.ivr_calls` calls
