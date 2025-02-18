0060fc040c4fac8856286de9284b05a977bb173b
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 19:32:13 +0100

    Update query8.sql

--- query8.sql
+++ query8.sql
@@ -1,18 +1,4 @@
-select 
-    ivr_calls.ivr_id,
-    ivr_calls.phone_number,
-    ivr_calls.ivr_result,
-    ivr_calls.vdn_label,
-    ivr_calls.start_date,
-    ivr_calls.end_date,
-    ivr_calls.total_duration,
-    ivr_calls.customer_segment,
-    ivr_calls.ivr_language,
-    ivr_calls.steps_module,
-    ivr_calls.module_aggregation,
-    case 
-        when module_aggregation like '%AVERIA_MASIVA%' then 1
-        else 0
-    end as masiva_lg
-from 
-    ivr_calls;
+SELECT ivr_id
+     , MAX(IF(module_name = "AVERIA_MASIVA", 1, 0)) AS masiva_lg
+  FROM keepcoding.ivr_detail
+ GROUP BY ivr_id
