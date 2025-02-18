1236eac7e4901be1a69c79d886715405b6268aa7
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 19:51:46 +0100

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,4 +1,9 @@
-create table Profesor (
+DROP TABLE IF EXISTS Profesor;
+DROP TABLE IF EXISTS Alumno;
+DROP TABLE IF EXISTS Bootcamp;
+DROP TABLE IF EXISTS Matricula;
+
+CREATE TABLE Profesor (
     id_profesor serial primary key,
     nombre varchar(50) not null,
     apellidos varchar(50) not null,
@@ -6,7 +11,7 @@ create table Profesor (
     email varchar(100) unique not null
 );
 
-create table  Alumno (
+CREATE TABLE  Alumno (
     id_alumno serial primary key,
     nombre varchar(50) not null,
     apellidos varchar(50) not null,
@@ -14,16 +19,19 @@ create table  Alumno (
     email varchar(100) unique not null
 );
 
-create table Bootcamp (
+CREATE TABLE Bootcamp (
     id_bootcamp serial primary key,
     nombre varchar(50) not null,
     id_profesor int not null,
     duracion int not null,
     numero_alumnos int not null,
     foreign key (id_profesor) references Profesor(id_profesor)
+    foreign key (id_alumno) references Alumno(id_alumno)
+    foreign key (id_bootcamp) references Bootcamp(id_bootcamp)
+    
+    
 );
-
-create table Matricula (
+CREATE TABLE Matricula (
     id_matricula serial primary key,
     id_alumno int not null,
     pago_al_contado boolean not null,
