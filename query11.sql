c382447fd7cc57deeb6eecf6ce75bcb2a7560474
gitbeatriz123 <velayosbea@gmail.com>
2025-02-18 19:29:06 +0100

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -1,36 +1,10 @@
-with ivr_calls_with_flags as (
-    select 
-        ivr_id,
-        phone_number,
-        ivr_result,
-        vdn_label,
-        start_date,
-        end_date,
-        total_duration,
-        customer_segment,
-        ivr_language,
-        steps_module,
-        module_aggregation,
-        case 
-            when exists (
-                select 1 
-                from ivr_calls as ic2 
-                where ic2.phone_number = ic1.phone_number 
-                and ic2.start_date between ic1.start_date - interval '24 HOURS' and ic1.start_date
-                and ic2.ivr_id != ic1.ivr_id
-            ) then 1 
-            else 0 
-        end as repeated_phone_24H,
-        case
-            when exists (
-                select 1 
-                from ivr_calls as ic3 
-                where ic3.phone_number = ic1.phone_number 
-                and ic3.start_date between ic1.start_date and ic1.start_date + interval '24 HOURS'
-                and ic3.ivr_id != ic1.ivr_id
-            ) then 1 
-            else 0 
-        end as cause_recall_phone_24H
-    from ivr_calls as ic1
-)
-select * from ivr_calls_with_flags;
+SELECT calls.ivr_id
+     , MAX(IF(DATE_DIFF(calls.start_date, recalls.start_date, SECOND) BETWEEN 1 AND 24*60*60, 1, 0)) AS repeated_phone_24H
+     , MAX(IF(DATE_DIFF(calls.start_date, recalls.start_date, SECOND) BETWEEN -24*60*60  AND -1, 1, 0)) AS cause_recall_phone_24H 
+  FROM keepcoding.ivr_detail calls
+  LEFT 
+  JOIN keepcoding.ivr_detail recalls
+    ON calls.phone_number <> 'UNKNOWN'
+   AND calls.phone_number = recalls.phone_number
+   AND calls.ivr_id <> recalls.ivr_id
+ GROUP BY ivr_id
